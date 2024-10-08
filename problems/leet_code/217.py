ad97422394576d7bb35593ecc72f5c77917ca347
tabibyte <tabib.hasanzadeh@gmail.com>
2024-10-08 04:12:39 +0400

    delete leetcode

--- problems/leet_code/217.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: list[int]) -> bool:
-        hash_map = {}
-        for num in nums:
-            if num in hash_map:
-                return True
-            hash_map[num] = None
-        return False
